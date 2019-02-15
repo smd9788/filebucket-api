aad717f9a30f61be38e535e94ea059308935ee4b
mverost <mverost4422@gmail.com>
2019-02-15 15:18:27 -0500

    Created Schema for Uploads and Tags. Added references to Upload schema for users and tags.

---
+++ app/models/tag.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const tagSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('tag', tagSchema)
---
+++ app/models/upload.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const uploadSchema = new mongoose.Schema({
+  url: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  tag: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Tag',
+    required: false
+  },
+  dateCreated: {
+    type: Date,
+    required: true
+  },
+  dateModified: {
+    type: Date,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('upload', uploadSchema)
