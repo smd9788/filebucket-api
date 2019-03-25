b9b86e8bd4685137d3c533db85598f16e5fc9507
smd9788 <smd09876@gmail.com>
2019-03-25 18:34:30 -0400

    fixed credentials

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // creating a base name for the mongodb
-const mongooseBaseName = 'file-bucket-api'
+const mongooseBaseName = 'filebucket-api'
 
 // create the mongodb uri for development and test
 const database = {
